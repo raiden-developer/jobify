9f9aa1a81801eb38337c0a5819fa8508d0154202
raiden-developer <raiden226@list.ru>
2022-08-15 03:45:20 +0800

    feat: landing page and first component

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,32 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* Info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas natus
+            iure distinctio praesentium, officia aspernatur eius ipsa mollitia
+            fuga consequuntur, magnam, itaque ipsum illo facilis corporis
+            inventore ullam esse repudiandae repellendus molestiae eaque sunt
+            maxime.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
