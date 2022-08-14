23cb3573072bba9d4a59b34742c8975fdec376e5
raiden-developer <raiden226@list.ru>
2022-08-15 04:28:37 +0800

    feat: error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,4 +1,17 @@
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Ohh! Page not found</h3>
+        <p>We can't seem fo find the page you're looking for</p>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 }
 export default Error;
